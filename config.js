7dbb27d6e5e9fef57ee34ce2ae421b62f199f9c7
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-09-29 00:21:09 +0200

    Create config.js
    definition of guessing elements

---
+++ config.js
@@ -0,0 +1,5 @@
+// list of guessing words
+const wordsToGuess = ["Cachalot","Pétunia","Serviette"]
+
+// list of guessing sentences
+const sentencesToGuess = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le présent."]
