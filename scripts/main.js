574cddcc393684c9ba5ddaef458f666018379507
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-09-29 11:53:53 +0200

    DOM handling

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,18 @@
 //start the game
-launchPlay()
+
+let inputWriting = document.getElementById("inputWriting")
+console.log(inputWriting)
+
+let buttonvalidateWord = document.getElementById("btnValidateWord")
+console.log(buttonvalidateWord)
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log(zoneProposition)
+
+let spanZoneScore = document.querySelector(".zoneScore span")
+console.log(spanZoneScore)
+
+let buttonradio = document.querySelectorAll(".optionSource input")
+console.log(buttonradio);
+
+// launchGame()
