fdc49cad442207665b573020f3225c1a08e28dec
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-10-06 18:15:52 +0200

    removal of unused variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
 //start the game
 
-let inputWriting = document.getElementById("inputWriting")
-console.log(inputWriting)
-
-let buttonvalidateWord = document.getElementById("btnValidateWord")
-console.log(buttonvalidateWord)
-
-let zoneProposition = document.querySelector(".zoneProposition")
-console.log(zoneProposition)
-
-let spanZoneScore = document.querySelector(".zoneScore span")
-console.log(spanZoneScore)
-
-let buttonradio = document.querySelectorAll(".optionSource input")
-console.log(buttonradio);
-
-// launchGame()
+launchGame()
