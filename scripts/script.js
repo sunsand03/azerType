87814cba63a288b6bb0a6c55a0f3390cef0f5cb0
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-10-10 11:21:45 +0200

    Update with variable listproposition(words or sentences)

--- scripts/script.js
+++ scripts/script.js
@@ -10,9 +10,26 @@ function displayresult(score,totalWords){
    spanScore.innerText = htmlScore
 }
 
-function displayWords(word){
-    let wordToGuess = document.querySelector(".zoneProposition")
-    wordToGuess.innerText = word
+/**
+ * displays the proposition to the user (sentences or words)
+ * @param {*} proposition 
+ */
+function displayPropositions(proposition){
+    let toGuess = document.querySelector(".zoneProposition")
+    toGuess.innerText = proposition
+}
+
+/**
+ * builds and displays the email. 
+ * @param {*} name : the name of the player
+ * @param {*} email : the email of the person with whom he wants to share his score
+ * @param {*} score : the score of the game
+ */
+function displayEmail(name, email, score) {
+    // build the mail
+    let mailto = `mailto:${email}?subject=Partage du score AzerType&body=Salut, je suis${name}  et je viens de réaliser le score de ${score} sur le site d'AzerType !`
+    // Navigate to the mailto URL to display the mail
+    location.href = mailto
 }
 
 /**
@@ -20,22 +37,34 @@ function displayWords(word){
  * @returns a sentence indicating the user’s score
  */
 function launchGame() {
-    // initialization
+
+       
+     // call of the function initializing the event management 
+     // once the document is fully loaded 
+     document.addEventListener('DOMContentLoaded', function() {
+     initAddEventListenerPopup();
+    });
+
+    // score initialization
     let score = 0
-    let totalWords = 0
+    
+    // counter initialization
     let i = 0
+
+    // by default, words are proposed
+    let listproposition = wordsToGuess
     
     // recovery of the validation button and the input field
     let buttonValidate = document.getElementById("btnValidateWord")
     let inputWriting = document.getElementById("inputWriting")
 
-    displayWords(wordsToGuess[i])
+    displayPropositions(listproposition[i])
 
     // recovery of the value entered at the click of the button
     buttonValidate.addEventListener("click", () =>{
             console.log(inputWriting.value)
             // update of the score in case of correct answer
-            if (inputWriting.value === wordsToGuess[i]){                
+            if (inputWriting.value === listproposition[i]){                
                 score++                
             }      
             i++
@@ -43,14 +72,34 @@ function launchGame() {
             inputWriting.value = ""  
             
             // end of the game when the word list is complete
-            if(wordsToGuess[i] === undefined){
-                displayWords("Le jeu est fini")
+            if(listproposition[i] === undefined){
+                displayPropositions("Le jeu est fini")
                 buttonValidate.disabled = true
             }else{
-                displayWords(wordsToGuess[i])
+                displayPropositions(listproposition[i])
             }
 
     })
+
+    //recovery of the radio button
+    let listBtnRadio = document.querySelectorAll(".optionSource input")
+
+    // listens to change at the radio buttons
+    for (let index = 0; index <listBtnRadio.length; index++){
+       listBtnRadio[index].addEventListener("change", (event)=>{
+        if(event.target.value === "1"){
+            listproposition = wordsToGuess
+        } else {
+            listproposition = sentencesToGuess
+            
+        }
+        displayPropositions(listproposition[i])
+        })
+    }
+    
+   
+
+
 
     // score display
     displayresult(score,i)
