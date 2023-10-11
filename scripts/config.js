9124a2b4a1a0e0937e4f5532287c6da82eca6350
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-10-11 18:51:45 +0200

    constant update

--- scripts/config.js
+++ scripts/config.js
@@ -5,8 +5,19 @@
 
 
 // list of guessing words
-const wordsToGuess = ["Cachalot","Pétunia","Serviette"]
+const wordsToGuess = ["Parallèle", "Dilemme","Cachalot","Pétunia","Serviette", "Accueil", "Kouign amann", "Reblochon", "Rhododendron", "trottinette"]
 
 // list of guessing sentences
-const sentencesToGuess = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le présent."]
+const sentencesToGuess = [
+    "On ne fait pas d'omelettes sans casser d'oeufs",
+    "On ne change pas une équipe qui gagne",
+    "Fais ce que je dis, ne fais pas ce que je fais",
+    "Il est facile de nager quand on vous tient le menton",
+    "Il ne faut pas jeter le bébé avec l'eau du bain",
+    "Jeu de main, jeu de vilain",
+    "Ce sont les tonneaux vides qui font le plus de bruit",
+    "Mieux vaut être seul que mal accompagné",
+    "On est jamais mieux servi que par soi-même",
+    "Qui vole un oeuf vole un boeuf"
+]
 
